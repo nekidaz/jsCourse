2d4cdafcf8bb50b4275c9cc908765c470d9164c8
Dilmurat <dlol96@mail.ru>
2022-06-16 16:13:52 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const userName=prompt("What is ur name?","");
+alert(`Hello ${userName}`);
+const userMood=prompt("How are you feeling today?","");
+
+alert(`Your mood is : ${userMood}`);
+
+const userOrientation=confirm("Are you gay ?");
+
+if(userOrientation==true){
+alert("You are pidor");
+}else {
+    alert("You are also gay");
+}
+
