7e42a7592cdb688541ddb6c806c60032c0827422
Dilmurat <dlol96@mail.ru>
2022-06-16 23:46:14 +0600

    Fist js programm

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict"
+const numberOfFilms = +prompt("Сколько фильсмов вы уже смотрели?", "");
+
+const personalMovie08 = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+const whichFilms = alert("Какие фильмы ?");
+
+const arrayFilms = [];
+
+for (let i = 0; i < numberOfFilms; i++) {
+    const filmsName = prompt(i + 1, "");
+    arrayFilms[i]=filmsName;
+}
+const filmsRatingsArray=[];
+for (let i = 0; i <numberOfFilms ; i++) {
+    const ratingFilms=prompt(`Рейтинг фильма: ${arrayFilms[i]}`,"");
+    filmsRatingsArray[i]=ratingFilms;
+}
+for (let i = 0; i <numberOfFilms ; i++) {
+    personalMovie08.movies[arrayFilms[i]]=filmsRatingsArray[i];
+}
+console.log(personalMovie08);
+
+
+
+
+
